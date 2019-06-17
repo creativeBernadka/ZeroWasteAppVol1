cac844ac64a91c269ead92072cc18e2a825b05ec
creativeBernadka <bfryczkowska@gmail.com>
2019-06-17 11:43:57 +0200

    ADD: getFiveNearestPlaces method added
    Getting five nearest to given location places added
    to ViewModel. Right now it is not tested.

--- app/src/main/java/com/kotlin/zerowasteappvol1/viewModel/PlacesViewModel.kt
+++ app/src/main/java/com/kotlin/zerowasteappvol1/viewModel/PlacesViewModel.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.content.Context
 import android.graphics.drawable.Drawable
+import com.google.android.gms.maps.model.LatLng
 import com.kotlin.zerowasteappvol1.database.Place
 import com.kotlin.zerowasteappvol1.database.PlaceDescription
 import com.kotlin.zerowasteappvol1.database.ShortPlace
@@ -13,8 +14,11 @@ interface PlacesViewModel {
     var allPlaces: MutableLiveData<List<ShortPlace>>
     var placeDescription: MutableLiveData<PlaceDescriptionWithAddress>
     var placeImages: MutableLiveData<List<Drawable?>>
+    var fiveNearestPlaces: MutableLiveData<List<ShortPlace>>
 
     fun getAllPlaces()
     fun getPlaceDescription(shortPlace: ShortPlace?, context: Context)
     fun getPlaceDetails(shortPlace: ShortPlace): LiveData<Place>
+    fun getFiveNearestPlaces(location: LatLng)
+    fun getFiveBestFittingPlaces(name: String)
 }
--- app/src/main/java/com/kotlin/zerowasteappvol1/viewModel/PlacesViewModelImpl.kt
+++ app/src/main/java/com/kotlin/zerowasteappvol1/viewModel/PlacesViewModelImpl.kt
@@ -4,8 +4,11 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.location.Location
+import com.google.android.gms.maps.model.LatLng
 import com.kotlin.zerowasteappvol1.database.Place
 import com.kotlin.zerowasteappvol1.database.PlaceDescription
 import com.kotlin.zerowasteappvol1.database.ShortPlace
@@ -21,7 +24,7 @@ class PlacesViewModelImpl @Inject constructor(application: Application, var repo
     override var allPlaces =  MutableLiveData<List<ShortPlace>>()
     override var placeDescription = MutableLiveData<PlaceDescriptionWithAddress>()
     override var placeImages = MutableLiveData<List<Drawable?>>()
-
+    override var fiveNearestPlaces =  MutableLiveData<List<ShortPlace>>()
     init {
         scope.launch(Dispatchers.IO) {
             allPlaces.postValue(async{repository.getAllPlacesAsync()}.await())
@@ -45,6 +48,28 @@ class PlacesViewModelImpl @Inject constructor(application: Application, var repo
         TODO()
     }
 
+    override fun getFiveBestFittingPlaces(name: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getFiveNearestPlaces(location: LatLng) {
+        val places = allPlaces.value
+        var distanceMarkerMap: HashMap<Float, ShortPlace> = HashMap()
+        val startLocation = Location("start location")
+        startLocation.latitude = location.latitude
+        startLocation.longitude = location.longitude
+
+        places?.forEach{ place ->
+            val endLocation = Location("end location")
+            endLocation.latitude = place.latitude
+            endLocation.longitude = place.longitude
+            distanceMarkerMap[startLocation.distanceTo(endLocation)] = place
+        }
+
+        val sortedPlaces = distanceMarkerMap.toSortedMap().values
+
+        fiveNearestPlaces.postValue(sortedPlaces.take(5))
+    }
 
 
 }
