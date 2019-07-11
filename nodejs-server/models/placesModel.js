e13d78612f2c87bf5a0416fe1c7263d37e21d240
creativeBernadka <bfryczkowska@gmail.com>
2019-07-11 13:36:25 +0200

    ADD: connection to database added to generated server

---
+++ nodejs-server/models/placesModel.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const config = require('../../config');
+
+let connection = mysql.createConnection({
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected! Jupi jej!");
+});
+
+module.exports = connection;
