4c396cbb5055b1171c00d36afa9e832064c4d583
creativeBernadka <bfryczkowska@gmail.com>
2019-07-17 15:20:49 +0200

    FIX: name of Default.js changed to Controller.js

--- nodejs-server/controllers/Controller.js
+++ nodejs-server/controllers/Controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const utils = require('../utils/writer.js');
-const Default = require('../service/DefaultServices');
+const Controller = require('../service/DefaultServices');
 
 module.exports.getAllPlaces = function getAllPlaces (req, res, next) {
-  Default.getAllPlaces(req, res);
+  Controller.getAllPlaces(req, res);
 };
 
 module.exports.getPlaceDescriptionById = function getPlaceDescriptionById (req, res, next) {
   const placeId = req.swagger.params['placeId'].value;
-  Default.getPlaceDescriptionById(req, res, placeId);
+  Controller.getPlaceDescriptionById(req, res, placeId);
 };
